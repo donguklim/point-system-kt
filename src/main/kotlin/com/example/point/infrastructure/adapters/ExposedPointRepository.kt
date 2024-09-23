03c3a401b5654e6bfda4728699c7f374fb732d92
KUDANSHI\dlim7 <dlim7272@gmail.com>
2024-09-23 23:09:25 +0900

    correctly working with flow and coroutine

--- src/main/kotlin/com/example/point/infrastructure/adapters/ExposedPointRepository.kt
+++ src/main/kotlin/com/example/point/infrastructure/adapters/ExposedPointRepository.kt
@@ -5,85 +5,130 @@ import com.example.point.domain.valueObjects.ChargedPoints
 import com.example.point.domain.valueObjects.ChargingPoints
 import com.example.point.domain.valueObjects.Consumption
 import com.example.point.infrastructure.database.PointDetails
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.withContext
-import kotlinx.datetime.Clock
-import kotlinx.datetime.DateTimeUnit
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.minus
-import kotlinx.datetime.toLocalDateTime
-import org.jetbrains.exposed.sql.SortOrder
+import kotlinx.datetime.*
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.greater
-import org.jetbrains.exposed.sql.alias
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.sum
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
-import org.jetbrains.exposed.sql.transactions.transaction
-import kotlin.coroutines.CoroutineContext
+import org.jetbrains.exposed.sql.transactions.experimental.withSuspendTransaction
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.sequences.Sequence
 
 class ExposedPointRepository(
     private val expiryDays: Int,
-    private val chargeBufferSize: Int = 100,
+    private val chunkSize: Int = 2000,
 ) : PointRepository {
-    override fun getPointFlow(userId: Int): Flow<ChargedPoints> =
-        flow {
-            val thresholdDateTime =
+    override fun getPointFlow(userId: Int): Flow<ChargedPoints> = flow {
+        var thresholdDateTime =
+            Clock.System.now().minus(
+                expiryDays,
+                DateTimeUnit.DAY,
+                TimeZone.UTC,
+            ).toLocalDateTime(TimeZone.UTC)
+        var thresholdChargeId = 0
+
+        val pointSum = PointDetails.numPoints.sum().alias("point_sum")
+
+        PointDetails.select(PointDetails.id).where(
+            (PointDetails.userId eq userId) and (PointDetails.expireAt greater thresholdDateTime),
+        ).forUpdate()
+
+        do {
+            var count = 0
+            val awaitingPoints = coroutineScope {
+                async {
+                    PointDetails.select(
+                        pointSum,
+                        PointDetails.chargeId,
+                        PointDetails.expireAt,
+                    ).where(
+                        (PointDetails.userId eq userId)
+                                and (
+                                (PointDetails.expireAt greater thresholdDateTime)
+                                        or (
+                                        (PointDetails.expireAt eq thresholdDateTime)
+                                                and (PointDetails.chargeId greater thresholdChargeId)
+                                        )
+                                ),
+                    ).groupBy(
+                        PointDetails.expireAt,
+                        PointDetails.chargeId,
+                    ).orderBy(
+                        PointDetails.expireAt to SortOrder.ASC,
+                        PointDetails.chargeId to SortOrder.ASC,
+                    ).limit(chunkSize).map {
+                        Pair(ChargedPoints(it[PointDetails.chargeId], it[pointSum] ?: 0), it[PointDetails.expireAt])
+                    }
+                }
+            }
+
+            awaitingPoints.await().forEach {
+                val (point, expireAt) = it
+                thresholdDateTime = expireAt
+                thresholdChargeId = point.chargeId
+                count++
+                if (point.getLeftPoints() > 0){
+                    println("flow ${userId}: ${thresholdChargeId} - ${point.getLeftPoints()}")
+                    emit(point)
+                }
+                
+            }
+        } while (count == chunkSize)
+        println("flow ${userId} end ")
+    }
+
+    override fun getPointSeq(userId: Int): Sequence<ChargedPoints> =
+        sequence {
+            val pointSum = PointDetails.numPoints.sum().alias("point_sum")
+            var thresholdDateTime =
                 Clock.System.now().minus(
                     expiryDays,
                     DateTimeUnit.DAY,
                     TimeZone.UTC,
                 ).toLocalDateTime(TimeZone.UTC)
 
-            val pointSum = PointDetails.numPoints.sum().alias("point_sum")
+            var thresholdChargeId = 0
 
-            PointDetails.select(
-                pointSum,
-                PointDetails.chargeId,
-                PointDetails.expireAt,
-            ).where(
+            PointDetails.select(PointDetails.id).where(
                 (PointDetails.userId eq userId) and (PointDetails.expireAt greater thresholdDateTime),
-            ).forUpdate().groupBy(
-                PointDetails.expireAt,
-                PointDetails.chargeId,
-            ).orderBy(PointDetails.expireAt to SortOrder.ASC).filter {
-                ((it[pointSum] ?: 0) > 0)
-            }.forEach {
-                emit(ChargedPoints(it[PointDetails.chargeId], it[pointSum] ?: 0))
-            }
+            ).forUpdate()
+
+            do {
+                var count = 0
+                PointDetails.select(
+                    pointSum,
+                    PointDetails.chargeId,
+                    PointDetails.expireAt,
+                ).where(
+                    (PointDetails.userId eq userId)
+                        and (
+                            (PointDetails.expireAt greater thresholdDateTime)
+                                or (
+                                    (PointDetails.expireAt eq thresholdDateTime)
+                                        and (PointDetails.chargeId greater thresholdChargeId)
+                                )
+                        ),
+                ).groupBy(
+                    PointDetails.expireAt,
+                    PointDetails.chargeId,
+                ).orderBy(
+                    PointDetails.expireAt to SortOrder.ASC,
+                    PointDetails.chargeId to SortOrder.ASC,
+                ).limit(chunkSize).forEach {
+                    thresholdDateTime = it[PointDetails.expireAt]
+                    thresholdChargeId = it[PointDetails.chargeId]
+                    count++
+                    if ((it[pointSum] ?: 0) > 0) {
+                        yield(ChargedPoints(it[PointDetails.chargeId], it[pointSum]!!))
+                        println("seq $userId: ${it[PointDetails.chargeId]} - ${it[pointSum]} ")
+                    }
+
+                }
+            } while (count == chunkSize)
         }
-
-
-    override fun getPointSeq(userId: Int): Sequence<ChargedPoints> {
-        val thresholdDateTime =
-            Clock.System.now().minus(
-                expiryDays,
-                DateTimeUnit.DAY,
-                TimeZone.UTC,
-            ).toLocalDateTime(TimeZone.UTC)
-
-        val pointSum = PointDetails.numPoints.sum().alias("point_sum")
-
-        return sequence {
-            PointDetails.select(
-                pointSum,
-                PointDetails.chargeId,
-                PointDetails.expireAt,
-            ).where(
-                (PointDetails.userId eq userId) and (PointDetails.expireAt greater thresholdDateTime),
-            ).forUpdate().groupBy(
-                PointDetails.expireAt,
-                PointDetails.chargeId,
-            ).orderBy(PointDetails.expireAt to SortOrder.ASC).filter {
-                ((it[pointSum] ?: 0) > 0)
-            }.forEach {
-                yield(ChargedPoints(it[PointDetails.chargeId], it[pointSum] ?: 0))
-            }
-        }
-    }
 
     override suspend fun updateCharges(
         userId: Int,
--- src/test/kotlin/com/example/point/infrastructure/adapters/ExposedPointRepositoryTests.kt
+++ src/test/kotlin/com/example/point/infrastructure/adapters/ExposedPointRepositoryTests.kt
@@ -4,10 +4,7 @@ import com.example.point.infrastructure.TestDatabase
 import com.example.point.infrastructure.database.PointDetails
 import com.example.point.infrastructure.database.PointEvents
 import com.example.point.infrastructure.database.PointType
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDateTime
@@ -26,6 +23,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertContains
 import kotlin.test.assertTrue
+import kotlin.time.measureTime
 
 data class PointData(
     val chargeId: Int,
@@ -179,143 +177,180 @@ class ExposedPointRepositoryTests {
             PointEvents.deleteAll()
         }
     }
+
     @Test
     fun pointFlow() {
-        val testUserId = 132
-        val charges: Map<Int, List<Int>> =
-            mapOf(
-                12343 to listOf(100, -20, -70),
-                11222 to listOf(3, -1, -1),
-                9333 to listOf(1),
-                8343 to listOf(3, -3),
-                5222 to listOf(33, -16, -17),
-            )
         val expireDays = 37
+        val testUserIds = (333..338).toList()
+        val userExpectedPoints: MutableMap<Int, Map<Int, Int>> = mutableMapOf()
+        for (testUserId in testUserIds) {
+            val charges: Map<Int, List<Int>> =
+                (1..20).associate {
+                    it to (List(11) { -1 } + listOf(11 + (0..20).random()))
+                }
 
-        val chargePointSums: Map<Int, Int> =
-            charges.mapValues {
-                it.value.sum()
-            }
+            val chargePointSums: Map<Int, Int> =
+                charges.mapValues {
+                    it.value.sum()
+                }
 
-        // charges should not have negative sum
-        chargePointSums.forEach { assertTrue(it.value >= 0) }
-        val expectedPoints = chargePointSums.filter { it.value > 0 }
+            chargePointSums.forEach { assertTrue(it.value >= 0) }
+            val expectedPoints = chargePointSums.filter { it.value > 0 }
+            userExpectedPoints[testUserId] = expectedPoints
 
-        val expiredCharges: Map<Int, List<Int>> =
-            mapOf(
-                8 to listOf(1232, -203, -730),
-                7 to listOf(3, -1, -1),
-                5 to listOf(10, -10),
-                3 to listOf(300, -3),
-            )
+            val expiredCharges: Map<Int, List<Int>> =
+                mapOf(
+                    3008 to listOf(1232 + (1..50).random(), -203, -730),
+                    3007 to listOf(3 + (1..50).random(), -1, -1),
+                    3005 to listOf(10 + (1..50).random(), -10),
+                    3003 to listOf(300 + (1..50).random(), -3),
+                )
 
-        // expired and not expired charge data charge id should not intersect
-        assertTrue(charges.keys.intersect(expiredCharges.keys).isEmpty())
+            assertTrue(charges.keys.intersect(expiredCharges.keys).isEmpty())
 
-        val expiredPointDataList =
-            expiredCharges.map {
-                val numDays = (1..19).random()
-                val expireAt =
-                    Clock.System.now().minus(
-                        expireDays + numDays,
-                        DateTimeUnit.DAY,
-                        TimeZone.UTC,
-                    )
-                it.value.map { numPoints ->
-                    PointData(
-                        chargeId = it.key,
-                        numPoints = numPoints,
-                        expireAt =
-                        expireAt.toLocalDateTime(TimeZone.UTC).let {
-                            LocalDateTime(it.year, it.month, it.dayOfMonth, it.hour, 0, 0, 0)
-                        },
-                        transactionAt =
-                        expireAt.minus(
-                            (1..9).random(),
+            val expiredPointDataList =
+                expiredCharges.map {
+                    val numDays = (1..19).random()
+                    val expireAt =
+                        Clock.System.now().minus(
+                            expireDays + numDays,
                             DateTimeUnit.DAY,
                             TimeZone.UTC,
-                        ).toLocalDateTime(TimeZone.UTC),
-                    )
-                }
-            }.flatten()
+                        )
+                    it.value.map { numPoints ->
+                        PointData(
+                            chargeId = it.key,
+                            numPoints = numPoints,
+                            expireAt =
+                                expireAt.toLocalDateTime(TimeZone.UTC).let {
+                                    LocalDateTime(it.year, it.month, it.dayOfMonth, it.hour, 0, 0, 0)
+                                },
+                            transactionAt =
+                                expireAt.minus(
+                                    (1..9).random(),
+                                    DateTimeUnit.DAY,
+                                    TimeZone.UTC,
+                                ).toLocalDateTime(TimeZone.UTC),
+                        )
+                    }
+                }.flatten()
 
-        val validPointDataList =
-            charges.map {
-                val numDays = (0..5).random()
-                val expireAt =
-                    Clock.System.now().minus(
-                        expireDays - numDays,
-                        DateTimeUnit.DAY,
-                        TimeZone.UTC,
-                    ).plus(
-                        1,
-                        DateTimeUnit.HOUR,
-                        TimeZone.UTC,
-                    )
-                it.value.map { numPoints ->
-                    PointData(
-                        chargeId = it.key,
-                        numPoints = numPoints,
-                        expireAt =
-                        expireAt.toLocalDateTime(TimeZone.UTC).let {
-                            LocalDateTime(it.year, it.month, it.dayOfMonth, it.hour, 0, 0, 0)
-                        },
-                        transactionAt =
-                        expireAt.minus(
-                            (1..9).random(),
+            val validPointDataList =
+                charges.map {
+                    val numDays = (0..5).random()
+                    val expireAt =
+                        Clock.System.now().minus(
+                            expireDays - numDays,
                             DateTimeUnit.DAY,
                             TimeZone.UTC,
-                        ).toLocalDateTime(TimeZone.UTC),
-                    )
-                }
-            }.flatten()
+                        ).plus(
+                            1,
+                            DateTimeUnit.HOUR,
+                            TimeZone.UTC,
+                        )
+                    it.value.map { numPoints ->
+                        PointData(
+                            chargeId = it.key,
+                            numPoints = numPoints,
+                            expireAt =
+                                expireAt.toLocalDateTime(TimeZone.UTC).let {
+                                    LocalDateTime(it.year, it.month, it.dayOfMonth, it.hour, 0, 0, 0)
+                                },
+                            transactionAt =
+                                expireAt.minus(
+                                    (1..9).random(),
+                                    DateTimeUnit.DAY,
+                                    TimeZone.UTC,
+                                ).toLocalDateTime(TimeZone.UTC),
+                        )
+                    }
+                }.flatten()
 
-        transaction {
-            // test point event
-            val testEventId =
-                PointEvents.insertAndGetId {
-                    it[userId] = testUserId
-                    it[transactionCode] = "test1"
-                    it[type] = PointType.CHARGE.value
-                    it[title] = "test"
-                    it[transactionAt] = Clock.System.now().toLocalDateTime(TimeZone.UTC)
-                }
+            transaction {
+                // test point event
+                val testEventId =
+                    PointEvents.insertAndGetId {
+                        it[userId] = testUserId
+                        it[transactionCode] = "test:$testUserId"
+                        it[type] = PointType.CHARGE.value
+                        it[title] = "test"
+                        it[transactionAt] = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+                    }
 
-            PointDetails.batchInsert(expiredPointDataList + validPointDataList) { pointInfo ->
-                this[PointDetails.userId] = testUserId
-                this[PointDetails.eventId] = testEventId
-                this[PointDetails.type] = if (pointInfo.numPoints >= 0) PointType.CHARGE.value else PointType.CONSUME.value
-                this[PointDetails.numPoints] = pointInfo.numPoints
-                this[PointDetails.chargeId] = pointInfo.chargeId
-                this[PointDetails.expireAt] = pointInfo.expireAt
-                this[PointDetails.transactionAt] = pointInfo.transactionAt
+                PointDetails.batchInsert(expiredPointDataList + validPointDataList) { pointInfo ->
+                    this[PointDetails.userId] = testUserId
+                    this[PointDetails.eventId] = testEventId
+                    this[PointDetails.type] = if (pointInfo.numPoints >= 0) PointType.CHARGE.value else PointType.CONSUME.value
+                    this[PointDetails.numPoints] = pointInfo.numPoints
+                    this[PointDetails.chargeId] = pointInfo.chargeId
+                    this[PointDetails.expireAt] = pointInfo.expireAt
+                    this[PointDetails.transactionAt] = pointInfo.transactionAt
+                }
             }
         }
 
         val repo =
             ExposedPointRepository(
                 expireDays,
+                chunkSize = 2,
             )
 
-        runBlocking {
-            // val singleThreadContext = newSingleThreadContext("SingleThread")
-            var chargeCount = 0
-            newSuspendedTransaction(Dispatchers.IO) {
+        val elaspedSync =
+            measureTime {
+                for (testUserId in testUserIds) {
+                    var chargeCount = 0
+                    assertContains(userExpectedPoints, testUserId)
+                    val expectedPoints = userExpectedPoints[testUserId]!!
+                    transaction {
+                        for (charge in repo.getPointSeq(testUserId)) {
+                            assertContains(
+                                expectedPoints,
+                                charge.chargeId,
+                                "The map should contain the key '${charge.chargeId}'",
+                            )
+                            assertEquals(expectedPoints[charge.chargeId], charge.getLeftPoints())
+                            chargeCount++
+                        }
+                    }
 
-                repo.getPointFlow(testUserId).collect { charge ->
-                    chargeCount++
-                    assertEquals(expectedPoints[charge.chargeId], charge.getLeftPoints())
+                    assertEquals(expectedPoints.size, chargeCount)
                 }
             }
 
-            assertEquals(expectedPoints.size, chargeCount)
+        println("Synchronous transaction Elapsed time: $elaspedSync")
 
-        }
+        val elapsed =
+            measureTime {
+                runBlocking {
+                    val jobs = mutableListOf<Job>()
 
+                    for (testUserId in testUserIds) {
+                        val job =
+                            launch {
+                                newSuspendedTransaction(Dispatchers.IO) {
+                                    assertContains(userExpectedPoints, testUserId)
+                                    val expectedPoints = userExpectedPoints[testUserId]!!
+                                    var chargeCount = 0
+                                    repo.getPointFlow(testUserId).collect { charge ->
+                                        chargeCount++
+                                        assertEquals(expectedPoints[charge.chargeId], charge.getLeftPoints())
+                                    }
+
+                                    assertEquals(expectedPoints.size, chargeCount, "${testUserId} does not have expected points in flow")
+                                }
+                            }
+                        jobs.add(job)
+                    }
+
+                    jobs.joinAll()
+                }
+            }
+        println("Suspended transaction Elapsed time: $elapsed")
 
         transaction {
             PointDetails.deleteAll()
             PointEvents.deleteAll()
         }
+
     }
 }
