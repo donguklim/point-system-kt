8a2929ae7e4da4212ce2f0b7855b7a6ad00eb81f
KUDANSHI\dlim7 <dlim7272@gmail.com>
2024-09-28 23:14:10 +0900

    Not using coroutine to fetch each chunk is faster, so the main reason to use Direct SQL is gone.

--- src/main/kotlin/com/example/point/infrastructure/adapters/ExposedPointRepository.kt
+++ src/main/kotlin/com/example/point/infrastructure/adapters/ExposedPointRepository.kt
@@ -42,70 +42,23 @@ class ExposedPointRepository(
 
             val pointSum = PointDetails.numPoints.sum().alias("point_sum")
 
-//            PointDetails.select(
-//                pointSum,
-//                PointDetails.chargeId,
-//                PointDetails.expireAt,
-//            ).where(
-//                (PointDetails.userId eq userId) and (PointDetails.expireAt greater thresholdDateTime)
-//            ).forUpdate().groupBy(
-//                PointDetails.expireAt,
-//                PointDetails.chargeId,
-//            ).orderBy(
-//                PointDetails.expireAt to SortOrder.ASC,
-//                PointDetails.chargeId to SortOrder.ASC,
-//            ).filter{
-//                (it[pointSum] ?: 0) > 0
-//            }.forEach {
-//                emit(ChargedPoints(it[PointDetails.chargeId], it[pointSum]!!))
-//            }
-
-            val connection = TransactionManager.current().connection
-
-            // Create a statement
-            val statement = connection.prepareStatement(
-                "SELECT sum(num_points) as point_sum, charge_id, expire_at FROM point_details " +
-                        "where user_id = ? and expire_at > ? group by expire_at, charge_id " +
-                        "order by expire_at, charge_id asc",
-                false
-            )
-
-            statement.fillParameters(
-                listOf(
-                    Pair(IntegerColumnType(), userId),
-                    Pair(KotlinLocalDateTimeColumnType(), thresholdDateTime)
-                )
-            );
-
-            // Set the fetch size
-            statement.fetchSize = chunkSize
-
-            // Execute your query
-            val resultSet = statement.executeQuery()
-
-            do {
-                var curChunkSize = 0
-                val awaitingPoints = coroutineScope {
-                    async {
-                        val points: MutableList<ChargedPoints> = mutableListOf()
-                        while (curChunkSize < chunkSize && resultSet.next()) {
-                            val point_sum =  resultSet.getInt("point_sum")
-                            curChunkSize++
-                            if (point_sum > 0) points.add(ChargedPoints(resultSet.getInt("charge_id"), point_sum))
-                        }
-                        points
-                    }
-                }
-
-                awaitingPoints.await().forEach { emit(it) }
-
-            } while (curChunkSize == chunkSize)
-
-//            while (resultSet.next()) {
-//                val point_sum =  resultSet.getInt("point_sum")
-//                if (point_sum > 0) emit(ChargedPoints(resultSet.getInt("charge_id"), point_sum))
-//            }
-
+            PointDetails.select(
+                pointSum,
+                PointDetails.chargeId,
+                PointDetails.expireAt,
+            ).where(
+                (PointDetails.userId eq userId) and (PointDetails.expireAt greater thresholdDateTime)
+            ).groupBy(
+                PointDetails.expireAt,
+                PointDetails.chargeId,
+            ).orderBy(
+                PointDetails.expireAt to SortOrder.ASC,
+                PointDetails.chargeId to SortOrder.ASC,
+            ).filter{
+                (it[pointSum] ?: 0) > 0
+            }.forEach {
+                emit(ChargedPoints(it[PointDetails.chargeId], it[pointSum]!!))
+            }
         }
 
     override fun getPointSeq(userId: Int): Sequence<ChargedPoints> =
@@ -118,53 +71,23 @@ class ExposedPointRepository(
                     TimeZone.UTC,
                 ).toLocalDateTime(TimeZone.UTC)
 
-//            PointDetails.select(
-//                pointSum,
-//                PointDetails.chargeId,
-//                PointDetails.expireAt,
-//            ).where(
-//                (PointDetails.userId eq userId) and (PointDetails.expireAt greater thresholdDateTime)
-//            ).forUpdate().groupBy(
-//                PointDetails.expireAt,
-//                PointDetails.chargeId,
-//            ).orderBy(
-//                PointDetails.expireAt to SortOrder.ASC,
-//                PointDetails.chargeId to SortOrder.ASC,
-//            ).filter{
-//                (it[pointSum] ?: 0) > 0
-//            }.forEach {
-//                yield(ChargedPoints(it[PointDetails.chargeId], it[pointSum]!!))
-//            }
-
-
-            val connection = TransactionManager.current().connection
-
-            // Create a statement
-            val statement = connection.prepareStatement(
-                "SELECT sum(num_points) as point_sum, charge_id, expire_at FROM point_details " +
-                        "where user_id = ? and expire_at > ? group by expire_at, charge_id " +
-                        "order by expire_at, charge_id asc",
-                false
-            )
-
-            statement.fillParameters(
-                listOf(
-                    Pair(IntegerColumnType(), userId),
-                    Pair(KotlinLocalDateTimeColumnType(), thresholdDateTime)
-                )
-            );
-
-            // Set the fetch size
-            statement.fetchSize = chunkSize
-
-            // Execute your query
-            val resultSet = statement.executeQuery()
-
-            while (resultSet.next()) {
-                val point_sum =  resultSet.getInt("point_sum")
-                if (point_sum > 0) yield(ChargedPoints(resultSet.getInt("charge_id"), point_sum))
+            PointDetails.select(
+                pointSum,
+                PointDetails.chargeId,
+                PointDetails.expireAt,
+            ).where(
+                (PointDetails.userId eq userId) and (PointDetails.expireAt greater thresholdDateTime)
+            ).groupBy(
+                PointDetails.expireAt,
+                PointDetails.chargeId,
+            ).orderBy(
+                PointDetails.expireAt to SortOrder.ASC,
+                PointDetails.chargeId to SortOrder.ASC,
+            ).filter{
+                (it[pointSum] ?: 0) > 0
+            }.forEach {
+                yield(ChargedPoints(it[PointDetails.chargeId], it[pointSum]!!))
             }
-
         }
 
     override suspend fun updateCharges(
