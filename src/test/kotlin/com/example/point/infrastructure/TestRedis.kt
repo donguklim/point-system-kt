8e29e7997f61900eb51e8578d8b833d8f6de8bac
KUDANSHI\dlim7 <dlim7272@gmail.com>
2024-12-02 22:10:32 +0900

    update docker compose

--- src/test/kotlin/com/example/point/infrastructure/TestRedis.kt
+++ src/test/kotlin/com/example/point/infrastructure/TestRedis.kt
@@ -5,7 +5,7 @@ import org.testcontainers.containers.GenericContainer
 object TestRedis {
     val password = "232a"
     val port = 6379
-    val redisContainer = GenericContainer<Nothing>("redis:latest").apply {
+    val redisContainer = GenericContainer<Nothing>("redis:7.4.1-alpine").apply {
         withExposedPorts(port)
         withEnv("REDIS_PASSWORD", password)
         start()
